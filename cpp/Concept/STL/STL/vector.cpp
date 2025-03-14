2851e9e6f9bbd9e567265b0b0c86fc091938fe0b
wslee <wslee@class-s.co.kr>
2025-03-14 14:26:48 +0900

    feat: Visual Studio 솔루션 및 프로젝트 파일 추가
    STL 및 MovingCircle 프로젝트를 위한 Visual Studio 솔루션 파일과 C++ 프로젝트 파일이 생성되었습니다.
    MFC 기반의 자식 뷰 클래스와 메인 프레임 클래스가 구현되었으며, 벡터를 사용하는 간단한 C++ 프로그램이 추가되었습니다.
    README.md 파일이 추가되어 MovingCircle 프로젝트에 대한 설명이 포함되었습니다.
    리소스 스크립트와 아이콘 파일도 추가되었습니다.

---
+++ cpp/Concept/STL/STL/vector.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if 1
+int main() {
+	vector<int> vec;
+	vec.push_back(10); // 맨 뒤에 10 추가
+}
+#endif
---
+++ mfc/HW/MovingCircle/MovingCircle/ChildView.cpp
@@ -0,0 +1,57 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MovingCircle.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+    CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+    CRect rect;
+    GetClientRect(&rect);
+
+    // 클라이언트 창 크기에 맞춰서 사각형의 크기를 설정
+    dc.Rectangle(rect.left + 10, rect.top + 10, rect.right - 500, rect.bottom - 10);
+}
+
